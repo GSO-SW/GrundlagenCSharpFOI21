3808d60d25bd31111e6d77e1773702a315b24796
Kander Akinci <k.akinci@gso.schule.koeln>
2022-09-29 18:04:56 +0200

    Aufgabe 2 eingefügt

---
+++ 1AufgabenLoesung/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+string v_name = "Avinash";
+string n_name = "Nilay";
+string alter = "17";
+
+Console.WriteLine($"{"Vorname:",-10}{v_name}");
+Console.WriteLine($"{"Nachname:",-10}{n_name}");
+Console.WriteLine($"{"Alter:",-10}{alter}");
---
+++ 2DatentypenKonvertierung/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine($"size of short: {short.MaxValue}");
+Console.WriteLine($"size of short: {short.MinValue}");
+/*
+ * Bit 16
+ size of short: 32767
+size of short: -32768
+ */
+
+
+string vorname = "Selim";
+short alter = 16;
+bool frage = true;
+
+int DollarZahl = 36;
+
+long Dollar = DollarZahl;
+
+char Dollar = DollarZahl;
