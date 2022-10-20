df78368dc1b285a9b5cff44482a27350fdccc8f0
Kander Akinci <k.akinci@gso.schule.koeln>
2022-10-20 11:58:16 +0200

    Einstieg für die Aufgabe umgesetzt

--- 1Konsolenausgabe-A2-N2/Program.cs
+++ 1Konsolenausgabe-A2-N2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string myFName_1 = "Fred";
+string myLName_1 = "Opals";
+int myInt_1 = 2456;
+
+string myFName_2 = "Betül";
+string myLName_2 = "Rötger";
+int myInt_2 = 2781;
+
+string waehrung = " EUR";
+
+
+Console.WriteLine($"{"Vorname",-10}| = |{myFName_1,-10}|");
+Console.WriteLine($"{"Gehalt",-10}| = |{myInt_1+waehrung,-10}|");
