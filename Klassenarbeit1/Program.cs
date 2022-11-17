efe7b21e2a9d12ce13e29202ddfd7ac9d0f8afd2
Kander Akinci <k.akinci@gso.schule.koeln>
2022-11-17 12:55:37 +0100

    ud

---
+++ Klassenarbeit1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+string sDM = "Durchmesser", sUB = "Umlaufbahn", sOF = "Oberfläche";
+double dDM = 12756.27, dUB = 149598022.96; int iOF = 510100000;
+
+Console.WriteLine($"{sDM,-11}{dDM,12:E2}\n");
+
+Console.WriteLine($"{sUB,-11}{dUB,12:E3}");
+
+Console.WriteLine();
+
+Console.WriteLine($"{sOF,-11}{iOF,12:E2}");
+
