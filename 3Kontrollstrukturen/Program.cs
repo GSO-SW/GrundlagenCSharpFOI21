b0aae8784a2cf7ff32eb6e672d6862a621f4c1c2
Kander Akinci <k.akinci@gso.schule.koeln>
2022-11-25 14:44:28 +0100

    UD

---
+++ 3Kontrollstrukturen/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+Console.WriteLine("Die kleinere Zahl\n");
+Console.WriteLine("Geben Sie die erste Zahl ein:");
+    
+double zahl1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Geben Sie die zweite Zahl ein:");
+double zahl2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("------------------");
+
+if (zahl1 == zahl2)
+{
+//ja
+
+}
+else
+{
+    //nein
+    if (zahl1 < zahl2) 
+    {
+        //ja
+    }
+    else
+    {
+        //nein
+    }
+
+}
